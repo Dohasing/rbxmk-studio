0b9e8433a8d78d091c2136a42a3d7d009087e4bb
Neemaxon <neemaxon@Neemaxons-MacBook-Air.local>
2026-02-28 19:42:00 +0500

    start new era of roblox technologies

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_rbxmk-studio_FILE /Users/neemaxon/Documents/rbxmk-studio/build/rbxmk-studio.app/Contents/MacOS/rbxmk-studio)
-set(__QT_DEPLOY_TARGET_rbxmk-studio_TYPE EXECUTABLE)
